06d52f9f40f18b1bd7f559741159c3b7172554cb
Ilya Osipov <osipovia85p@gmail.com>
2019-07-11 23:21:21 +0300

    Editing Tasks #1

--- Conditions if, else, elif. Blocks, indents/conditions_if_else_elif_blocks_indents-leap_year.py
+++ Conditions if, else, elif. Blocks, indents/conditions_if_else_elif_blocks_indents-leap_year.py
@@ -6,3 +6,12 @@ elif year % 4 == 0 and year % 400 == 0:
     print("Високосный")
 else:
     print("Обычный")
+
+
+
+year = int(input())
+
+if year % 4 == 0 and year % 100 > 0 or year % 400 == 0:
+   print("Високосный")
+else:
+   print("Обычный")
